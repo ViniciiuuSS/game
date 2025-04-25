2b5079e1df13aa60ad881ed9c2df8a657f18286c
vinicius guedes <vinicraft556@gmail.com>
2025-04-24 23:08:03 -0300

    Fix - Correção cor

--- src/components/automatizarColetas.tsx
+++ src/components/automatizarColetas.tsx
@@ -9,8 +9,8 @@ export default function AutomatizarColetas() {
     <div className="container-buttons flex flex-row gap-4">
         <button className="retro-button text-lg red-button" onClick={() => automatizarColetas("A", false)} disabled={score < precoAutomatizarColetas.A || statusAutoButtons.A}><ArrowPathIcon className="w-15 h-5 text-green-300" /> A</button>
         <button className="retro-button text-lg yellow-button" onClick={() => automatizarColetas("B", false)} disabled={score < precoAutomatizarColetas.B || statusAutoButtons.B}><ArrowPathIcon className="w-15 h-5 text-green-300" /> B</button>
-        <button className="retro-button text-lg green-button" onClick={() => automatizarColetas("X", false)} disabled={score < precoAutomatizarColetas.X || statusAutoButtons.X}><ArrowPathIcon className="w-15 h-5 text-green-300" /> X</button>
-        <button className="retro-button text-lg blue-button" onClick={() => automatizarColetas("Y", false)} disabled={score < precoAutomatizarColetas.Y || statusAutoButtons.Y}><ArrowPathIcon className="w-15 h-5 text-green-300" /> Y</button>
+        <button className="retro-button text-lg blue-button" onClick={() => automatizarColetas("X", false)} disabled={score < precoAutomatizarColetas.X || statusAutoButtons.X}><ArrowPathIcon className="w-15 h-5 text-green-300" /> X</button>
+        <button className="retro-button text-lg green-button" onClick={() => automatizarColetas("Y", false)} disabled={score < precoAutomatizarColetas.Y || statusAutoButtons.Y}><ArrowPathIcon className="w-15 h-5 text-green-300" /> Y</button>
     </div>
   );
 }
